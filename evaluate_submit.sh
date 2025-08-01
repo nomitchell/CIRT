3a952a5f1447ddcd0249ab29f194cb99a3e11afc
Noah Mitchell <noah@gorobmitchell.com>
2025-08-01 11:11:58 -0400

    version

--- evaluate_submit.sh
+++ evaluate_submit.sh
@@ -4,12 +4,12 @@
 #SBATCH --gpus-per-node=v100l:4
 #SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=72-00:00
+#SBATCH --time=00-00:30
 #SBATCH --output=HPO-cifar-%A-%a.out
 #SBATCH --mail-user=nmitc082@uottawa.ca
 #SBATCH --mail-type=ALL
 
-module load python/<3.12> # Make sure to choose a version that suits your application
+module load python
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install torch torchvision --no-index
